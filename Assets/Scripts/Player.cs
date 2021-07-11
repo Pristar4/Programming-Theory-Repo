b0186b5b98e326e6438a5dc6d8dcde73fc8eaa3f
Felix <felix.j.jung@gmail.com>
2021-07-11 12:41:31 +0200

    Aim Movment fix

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Player : Enemy
 {
 
-    
+    public Rigidbody Rb;
+    public GameObject target;
 
+    private Vector2 movement;
     private void Start()
     {
         _currenthealth = _maxhealth;
@@ -15,6 +18,10 @@ public class Player : Enemy
 
     private void Update()
     {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        
+        
         if (_currenthealth > 0)
         {
             isdead = false;
@@ -27,6 +34,25 @@ public class Player : Enemy
         {
             isdead = true;
         }
+
+        if (Input.GetButton("Fire1"))
+        {
+            
+            Shoot();
+          
+            
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        var spetim = Speed * Time.deltaTime;
+        Rb.velocity = new Vector3(movement.x *spetim,Rb.velocity.y,movement.y* spetim);
+    }
+
+    private void Shoot()
+    {
+        
     }
 
     
